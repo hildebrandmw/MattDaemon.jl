d7222d941d185e01aec0fe9ee5ac136e78a6abaf
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-03-18 16:03:45 -0700

    Files generated by PkgTemplates

---
+++ src/MattDaemon.jl
@@ -0,0 +1,5 @@
+module MattDaemon
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MattDaemon
+using Test
+
+@testset "MattDaemon.jl" begin
+    # Write your own tests here.
+end
